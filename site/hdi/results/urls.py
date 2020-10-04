b967ae9dfb23dc80f6696bfc52993baf3f7ddc81
pascia <erenstarter@gmail.com>
2020-10-04 15:45:21 +0300

    Update

--- site/hdi/results/urls.py
+++ site/hdi/results/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-url_patterns = [
-    path("", views.index)
-    path("results/<str>", views.results)
+urlpatterns = [
+    path("", views.index),
+    path("<str:link>", views.results)
 ]
--- site/hdi/results/views.py
+++ site/hdi/results/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    return render(request, "pages/form.html")
 
-def index(results):
+def results(request, link):
+
+    from ipware import get_client_ip
+    ip, is_routable = get_client_ip(request)
+
+    return render(request, "pages/results.html",{
+    "link":link
+    })
